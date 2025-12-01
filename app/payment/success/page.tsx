0807232a99f090dc09c637d41f3dfc3ff44c7fb7
Soham Dutta <sohamdutta225@gmail.com>
2025-12-01 10:25:47 +0530

    success url cahnged

--- app/payment/success/page.tsx
+++ app/payment/success/page.tsx
@@ -1,31 +1,32 @@
-import { Require_User } from "@/app/utils/requireUser";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { Check } from "lucide-react";
-import Link from "next/link";
+import { prisma } from "@/app/utils/db";
+import { stripe } from "@/app/utils/stripe";
+import { redirect } from "next/navigation";
 
-export default async function PaymentSuccess() {
-  await Require_User();
+export default async function PaymentSuccess({ searchParams }: any) {
+  const session_id = searchParams.session_id;
+
+  if (!session_id) {
+    return redirect("/");
+  }
+
+  // Fetch full stripe session
+  const session = await stripe.checkout.sessions.retrieve(session_id);
+
+  const jobId = session.metadata?.jobId;
+
+  if (!jobId) {
+    return redirect("/");
+  }
+
+  // Mark the job as ACTIVE
+  await prisma.jobPost.update({
+    where: { id: jobId },
+    data: { status: "ACTIVE" },
+  });
+
   return (
     <div className="w-full min-h-screen flex flex-1 justify-center items-center">
-      <Card className="w-[350px]">
-        <div className="p-6">
-          <div className="w-full flex justify-center">
-            <Check className="size-12 p-2 bg-green-500/30 text-green-500 rounded-full" />
-          </div>
-          <div className="w-full mt-3 text-center sm:mt-5">
-            <h2 className="font-semibold text-xl">Payment Successful</h2>
-            <p className="text-sm mt-2 text-muted-foreground tracking-tight text-balance">
-              Congratulations! your payment was successful. Your job posting is
-              now active
-            </p>
-
-            <Button asChild className="w-full mt-5">
-              <Link href={"/"}>Go Back To Homepage</Link>
-            </Button>
-          </div>
-        </div>
-      </Card>
+      <h1 className="text-2xl font-bold">Payment Successful! 🎉</h1>
     </div>
   );
 }
